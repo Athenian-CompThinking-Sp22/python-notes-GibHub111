26626a0ce2ffd85984296318385bbdeba598db82
kliqs <25gkolis@gmail.com>
2022-04-07 12:30:13 -0700

    D period day one finished!

--- src/print_input.py
+++ src/print_input.py
@@ -4,5 +4,26 @@
 #
 #
 #############################
+# import time
+#
+# # Write code here:
+# print("Hello World!")
+# time.sleep(1)
+# print("Milo is a monkey.")
+#
+# # name = "Florb"
+# # print(f"Hello {name}!")
+#
+# time.sleep(2)
+# name = input("Please enter your name: ")
+# print(f"Hello {name}!")
+# time.sleep(2)
+# print("Florb is a very stupid name!")
 
-# Write code here:
+x = 8
+y = 7
+print(x ** y)
+num1 = int(input("Enter a number: "))
+num2 = int(input("Enter another number: "))
+num3 = int(input("Enter a third number: "))
+print(num1 ** num2/num3)
